c6418d78a6d7c97d1152ab0c3ee5c4a69213774f
RufZero <victor.p17@outlook.com>
2025-05-23 13:25:31 +0000

    Nivel Intermediario

--- xadrez.c
+++ xadrez.c
@@ -3,13 +3,14 @@
 int main() {
     
     
-    int i;
+    int i, j;
+    
 
     // Movimento da Torre: 5 casas para a direita (usando for)
   
     printf("Movimento da Torre:\n");
     for(i = 1; i <= 5; i++) {
-        printf("Casa %d: Direita\n", i);
+        printf("Direita\n");
     }
 
     // Movimento do Bispo: 5 casas na diagonal para cima e à direita (usando while)
@@ -17,7 +18,7 @@ int main() {
     printf("\nMovimento do Bispo:\n");
     i = 1;
     while(i <= 5) {
-        printf("Casa %d: Cima, Direita\n", i);
+        printf("Cima, Direita\n");
         i++;
     }
 
@@ -26,9 +27,27 @@ int main() {
     printf("\nMovimento da Rainha:\n");
     i = 1;
     do {
-        printf("Casa %d: Esquerda\n", i);
+        printf("Esquerda\n");
         i++;
     } while(i <= 8);
+
+    printf("\n");
+
+    // Movimento do Cavalo: 2 casas para baixo e 1 casa para a esquerda (usando for)
+  
+    printf("Movimento do Cavalo:\n");
+
+    for(i = 0; i < 2; i++){
+
+            printf("Cima\n");
+
+        for (j = 1; j <= i; j++)
+        {
+            printf("Direita\n");
+            j++;
+        }
+
+    }
 
 
     return 0;
